600a3715e343fdba12d933e3fb80e3dd9b566eda
miva2 <miva2-_-@hotmail.com>
2022-06-03 21:47:16 +0200

    Created Dialogue scriptable object
    This is used to write all the cues of a character. currently this is very simple and probably only good for sequential dialogue.

---
+++ TobuJam2022Unity/Assets/Scripts/Dialogue.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "ScriptableObjects/Dialogue", order = 1)]
+public class Dialogue : ScriptableObject
+{
+    public string[] cues;
+
+}
